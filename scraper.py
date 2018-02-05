1096b14b8825d49f61c6c3cc3ffde6a580426789
Robert Gieseke <robert.gieseke@pik-potsdam.de>
2018-02-05 11:40:46 +0100

    Add Doha submodule and scraper

--- scraper.py
+++ scraper.py
@@ -5,10 +5,18 @@ subprocess.run([
     "scripts/process.py"
 ], cwd="paris-agreement-entry-into-force")
 
+subprocess.run([
+    "python",
+    "scripts/process.py"
+], cwd="doha-amendment-entry-into-force")
+
 
 subprocess.run([
     "csvs-to-sqlite",
     "--replace-tables",
-    "paris-agreement-entry-into-force/data/paris-agreement-entry-into-force.csv",
+    ("paris-agreement-entry-into-force/data/"
+     "paris-agreement-entry-into-force.csv"),
+    ("doha-amendment-entry-into-force/data/"
+     "doha-amendment.csv"),
     "data.sqlite"
 ])
